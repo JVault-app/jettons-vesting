49935f615e798eeeee3f8d57064f68c76b6f4fbf
Arkadiy Stena <arkadiystena@gmail.com>
2024-06-23 01:38:59 +0300

    mainnet v0

--- scripts/changeFee.ts
+++ scripts/changeFee.ts
@@ -3,6 +3,6 @@ import { compile, NetworkProvider } from '@ton/blueprint';
 import { Factory } from '../wrappers/Factory';
 
 export async function run(provider: NetworkProvider) {
-    const factory = provider.open(Factory.createFromAddress(Address.parse("EQD1b2tHSkG9TNf5SrO1tmgfYV8Tl6ZoEpON57xe4f-sQNQo")))
+    const factory = provider.open(Factory.createFromAddress(Address.parse("EQADYRiavRRe-eh4D-VjIJp15MPxMi5rGIWzk4ClX1zBvYI5")))
     await factory.sendChangeCreationFee(provider.sender(), toNano("0.5"))
 }
--- scripts/withdrawTon.ts
+++ scripts/withdrawTon.ts
@@ -3,6 +3,6 @@ import { compile, NetworkProvider } from '@ton/blueprint';
 import { Factory } from '../wrappers/Factory';
 
 export async function run(provider: NetworkProvider) {
-    const factory = provider.open(Factory.createFromAddress(Address.parse("kQAFpxXEHwPpjzIdxSEoFtfkkx1w9O8Yzr7SApq2vd6HCbsv")))
+    const factory = provider.open(Factory.createFromAddress(Address.parse("EQADYRiavRRe-eh4D-VjIJp15MPxMi5rGIWzk4ClX1zBvYI5")))
     await factory.sendWithdrawTon(provider.sender())
 }
